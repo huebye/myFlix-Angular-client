432892f3ad33210066cc347c75575accadbd92ae
huebye <thomashuebner94@gmx.de>
2021-08-02 17:03:34 +0200

    added login and. registration form using angular material

---
+++ src/app/user-login-form/user-login-form.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-user-login-form',
+  templateUrl: './user-login-form.component.html',
+  styleUrls: ['./user-login-form.component.scss']
+})
+export class UserLoginFormComponent implements OnInit {
+
+  @Input() userData = { Name: '', Password: ''};
+
+constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar) { }
+
+ngOnInit(): void {
+}
+
+// This is the function responsible for sending the form inputs to the backend
+loginUser(): void {
+    this.fetchApiData.userLogin(this.userData).subscribe((result) => {
+      localStorage.setItem('token', result.token);
+      localStorage.setItem('user', result.user.Name);
+  // Logic for a successful user registration goes here! (To be implemented)
+     this.dialogRef.close(); // This will close the modal on success!
+     console.log(result)
+     this.snackBar.open(result, 'OK', {
+        duration: 2000
+     });
+    }, (result) => {
+      this.snackBar.open(result, 'OK', {
+        duration: 2000
+      });
+    });
+  }
+
+  }
